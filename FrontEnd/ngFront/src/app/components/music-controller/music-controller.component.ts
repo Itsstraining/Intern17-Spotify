3087da3479d71ebc8d2203ef2d644ed96212cbd4
Dương Gia Lạc <lac.dg@itsstraining.edu.vn>
2019-08-10 13:25:24 +0700

    Project template

---
+++ FrontEnd/ngFront/src/app/components/music-controller/music-controller.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-music-controller',
+  templateUrl: './music-controller.component.html',
+  styleUrls: ['./music-controller.component.scss']
+})
+export class MusicControllerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
