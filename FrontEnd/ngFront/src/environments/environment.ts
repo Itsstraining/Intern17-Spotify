63ea0f570af0e36ca1e0c94b597486b2047ec447
unknown <nam.tp@itsstraining.edu.vn>
2019-08-20 09:12:17 +0700

    add

--- FrontEnd/ngFront/src/environments/environment.ts
+++ FrontEnd/ngFront/src/environments/environment.ts
@@ -3,9 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyAjHFm96VQI_ZGWf1Y2OIqvDrhMNhIxz3o",
+    authDomain: "hack-79219.firebaseapp.com",
+    databaseURL: "https://hack-79219.firebaseio.com",
+    projectId: "hack-79219",
+    storageBucket: "hack-79219.appspot.com",
+    messagingSenderId: "583288035638",
+    appId: "1:583288035638:web:9c26351552aad8f3"
+  }
 };
-
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
